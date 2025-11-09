3a60bb617d9fcb72d8648f6c1184401d464a9e85
khuramsultan947-arch <khuramsultan947@gmail.com>
2025-11-09 12:19:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const staticConfig = {
     // ==================== BAILEYS & SESSION ====================
     BAILEYS: "@whiskeysockets/baileys",
-    SESSION_ID: process.env.SESSION_ID || "EDITH~dfHG",  // Keep session ID in plain text
+    SESSION_ID: process.env.SESSION_ID || "EDITH~SBkN",  // Keep session ID in plain text
     CDN: process.env.CDN || "https://cdn-bandaheali.zone.id",
 };
 
